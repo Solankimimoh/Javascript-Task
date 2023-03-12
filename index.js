b5b1a01eae87b1e5ba5c042e19ce86fdba9910b1
Mimoh Solanki <solankimimoh@gmail.com>
2023-03-12 07:28:58 +0000

    complete armstrong check and armstrong print from 1 to N

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 const scanner = require("prompt-sync");
 
 function init() {
-  console.log("Welcome to task list :");
+  console.log("\nWelcome to task list : \n");
   console.log("1. Find factorial of a number.");
   console.log("2. Display multiplication table.");
   console.log("3. Print the fibonacci sequence.");
   console.log("4. Check armstrong number.");
-  let choice = prompt("Please enter your choice:");
+  console.log("4. Print armstrong number from interval. \n");
+  let choice = parseInt(prompt("Please enter your choice:"));
 
   choiceSelection(choice);
 
 }
 
-function choiceSelection() {
+function choiceSelection(choice) {
   switch (choice) {
     case 1:
       factorialNum();
+      init();
       break;
     case 2:
       multiplicationTable();
+      init();
       break;
     case 3:
       fibonacciSeries();
+      init();
       break;
     case 4:
+      armstrongNumVerify();
+      init();
       break;
     case 5:
+      armstrongNumInterval();
+      init();
       break;
     default:
-      console.log("Please enter valid choice.. Try again!");
+      console.log(" \n Please enter valid choice.. Try again!");
       init();
       break;
   }
@@ -72,15 +80,69 @@ function fibonacciSeries() {
 
   for (let i = 0; i < num; i++) {
 
-  
+
     console.log(prevNum);
-  
-    let sum = prevNum+nextNum;
-    prevNum=nextNum;
-    nextNum=sum;
+
+    let sum = prevNum + nextNum;
+    prevNum = nextNum;
+    nextNum = sum;
   }
 
 
 }
 
-fibonacciSeries();
+// This function will help to check given number is armstrong or not.
+function armstrongNumVerify() {
+  let num = prompt("Enter the number to check armstrong number.");
+
+  if (armstrongCheck(num)) {
+    console.log(num + " is armstrong");
+  } else {
+    console.log(num + " is not armstrong");
+  }
+
+}
+
+// This function will help to print amrstrong number from internval
+function armstrongNumInterval() {
+  let num = prompt("Enter the end number to check armstrong number interval.");
+
+  for (let i = 1; i <= num; i++) {
+    if (armstrongCheck(i)) {
+      console.log(i + " is armstrong");
+    } else {
+      console.log(i + " is not armstrong");
+    }
+  }
+}
+
+// This is function will take number as input and calculate and check armstrong number return true or false
+function armstrongCheck(num) {
+
+  let powerCount = num.toString().length;
+  let armstrongNum = 0;
+  let originalNum = num;
+
+  for (let i = 0; i < powerCount; i++) {
+
+    let reminder = Math.floor(num % 10);
+    let tempNum = num / 10;
+    num = tempNum;
+    let powerCal = 1;
+
+    for (let j = 0; j < powerCount; j++) {
+      powerCal = powerCal * reminder;
+    }
+    armstrongNum = armstrongNum + powerCal;
+
+  }
+
+  if (armstrongNum == originalNum) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+init();
