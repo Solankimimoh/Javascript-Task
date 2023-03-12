5f13f8b08dd99e9ee675efbd1cf8a04c34e899f1
Mimoh Solanki <solankimimoh@gmail.com>
2023-03-12 04:02:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const scanner = require("prompt-sync");
+
+function init(){
+  console.log("Welcome to task list :");
+  console.log("1. Find factorial of a number.");
+  console.log("2. Display multiplication Number.");
+  console.log("3. Print the fibonacci sequence.");
+  console.log("4. Check armstrong number.");
+  let choice = prompt("Please enter your choice:");
+
+  choiceSelection(choice);
+  
+}
+
+function choiceSelec
+
+
+init();
